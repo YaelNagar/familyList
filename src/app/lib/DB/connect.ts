cdbfc9541446939f9a651498ee4c7c28e7368688
Chani Cohen <72072ch@gmail.com>
2024-11-21 12:02:09 +0200

    vercel

--- src/app/lib/DB/connect.ts
+++ src/app/lib/DB/connect.ts
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
-const MONGODB_URI = process.env.MONGO_URI || "";
+// import mongoose from "mongoose";
+// const MONGODB_URI = process.env.MONGO_URI || "";
 
-const connect = async () => {
-    try {
-        await mongoose.connect(MONGODB_URI);
-        console.log("connect mongoDB");
-    }catch(error){
-        throw new Error("Error in connecting to mongoDB");
-    }
-};
+// const connect = async () => {
+//     try {
+//         await mongoose.connect(MONGODB_URI);
+//         console.log("connect mongoDB");
+//     }catch{
+//         throw new Error("Error in connecting to mongoDB");
+//     }
+// };
 
-export default connect;
+// export default connect;
